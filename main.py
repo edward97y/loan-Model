082ada30682f110db17b976b3c8c22a53e6d41c8
Edwar Darani <134034799+edward97y@users.noreply.github.com>
2026-01-29 12:24:38 +0300

    Initial project setup

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from pydantic import BaseModel,field_validator
+from contextlib import asynccontextmanager
+import pandas as pd
+import joblib
+
+
+class Features(BaseModel):
+    no_of_dependents:int
+    education:str
+    self_employed:str
+    income_annum:int
+    loan_amount:int
+    loan_term:int
+    cibil_score:int
+    residential_assets_value:int
+    commercial_assets_value:int
+    luxury_assets_value:int
+    bank_asset_value:int
+
+    
+    @field_validator('education')
+    def check_education(cls,v):
+        if v.lower() not in ("graduate","not graduate"):
+            raise ValueError("education must be Graduate or Not Graduate")
+        return v.lower()
+    
+    @field_validator('self_employed')
+    def check_employed(cls,v):
+        if v.lower() not in ("yes","no"):
+            raise ValueError("this field must be yes or no")
+        return v.lower()
+
+@asynccontextmanager
+async def load_model(app:FastAPI):
+    app.state.model=joblib.load('pipeline_model.pkl')
+    yield
+      
+app=FastAPI(lifespan=load_model)
+@app.post('/')
+def predict_loan(features:Features):
+    try:
+       model=app.state.model
+       data=pd.DataFrame([features.dict()])
+       data.columns = [" " + col for col in data.columns]
+       predict=model.predict(data)[0]
+       return {"prediction":predict.strip()}
+    except Exception as e:
+        return {"error": str(e)}
+
